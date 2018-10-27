ddeee64df905f92b4e824a86587ba417a6a5f399
Andrew-64 <31630789+Andrew-64@users.noreply.github.com>
2018-10-27 16:51:45 -0400

    Test Cases to Master

--- frontend/tests.py
+++ frontend/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
+from django.test import Client
+from .views import *
 
-# Create your tests here.
+
+#Tests if client can access the site
+class TestAccess(TestCase):
+    def test_checkin_is_loaded(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+#Tests if client can access .../view_map/
+class TestMapAccess(TestCase):
+    def test_map_is_loaded(self):
+        response = self.client.get('/view_map/')
+        self.assertEqual(response.status_code, 200)
+
+#Tests if client can access .../check_in/
+class TestCheckinAccess(TestCase):
+    def test_checkin_is_loaded(self):
+        response = self.client.get('/check_in/')
+        self.assertEqual(response.status_code, 200)
+
+
+##class CheckForCredentials(TestCase):
+##    def setUp(self):
+##	#Do things
+##
+##    def test_check_for_credentials(self):
+##	#Do more things
+##
+##
+##
+##class CheckOccupancyCount(TestCase):
+##    def check_in_empty_room(self):
+##	#Room count = 0
+##	#check_in()
+##	#Test(Room count == 1)
+##
+##    def check_out_room(self):
+##	#Room count = 10
+##	#check_out()
+##	#Test(Room count == 9)
+
+
+
