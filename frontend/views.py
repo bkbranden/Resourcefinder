4361d5b995ed8e38497d6af5800be1c05617dd3a
bkbranden <bk2dh@virginia.edu>
2018-10-29 16:00:15 -0400

    postrequests

--- frontend/views.py
+++ frontend/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Location
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Student
+from .forms import UserForm
 
 def index(request):
     return render(request, 'frontend/index.html')
 
 def check_in(request):
-    locations = Location.objects.all()
-    return render(request, 'frontend/check_in.html', { 'locations': locations})
+   
+    return render(request, 'frontend/check_in.html')
 
 def view_map(request):
     mapbox_access_token = 'pk.eyJ1IjoiYWJzdXJkdmFjYXRpb24iLCJhIjoiY2puamxqNXV2MG4yeDNwbGs1MmozcDZvdCJ9.AWfhzxC6hwtwrq8yFbfBOA'
     return render(request, 'frontend/view_map.html', { 'mapbox_access_token': mapbox_access_token })
+
+def viewusers(request):
+    students = Student.objects.all()
+    return render(request, 'frontend/viewusers.html', {'students': students})
+
+# def get_user(request):
+#     if (request.method == "POST"):
+#         temp = request.POST.get("username", "")
+#         # student_instance = Student.objects.update_or_create(student_name = request.POST['username'])
+#         # student_instance.save()
+#         # return HttpResponseRedirect("frontend/viewusers.html")
+#     return HttpResponse(temp)
+#     # return render(request, 'frontend/viewusers.html')
