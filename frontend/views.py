449a93b09730c7bfd980b6ffe6b66b085a928d08
bkbranden <bk2dh@virginia.edu>
2018-10-28 22:35:31 -0400

    model to view testing

--- frontend/views.py
+++ frontend/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Location
 
 def index(request):
     return render(request, 'frontend/index.html')
 
 def check_in(request):
-    return HttpResponse("Hello, world. Are you ready to check into a library?")
+    locations = Location.objects.all()
+    return render(request, 'frontend/check_in.html', { 'locations': locations})
 
 def view_map(request):
     mapbox_access_token = 'pk.eyJ1IjoiYWJzdXJkdmFjYXRpb24iLCJhIjoiY2puamxqNXV2MG4yeDNwbGs1MmozcDZvdCJ9.AWfhzxC6hwtwrq8yFbfBOA'
