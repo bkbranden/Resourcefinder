fd32ca62f48b23a92d3d982b7ba92626db4ea3f3
nc3tq <nc3tq@virginia.edu>
2018-10-29 01:55:44 -0400

    marker does not work

--- manage.py
+++ manage.py
@@ -3,11 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     os.environ.setdefault('DJANGO_SETTINGS_MODULE','absurdvacations.settings')
-=======
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'absurdvacations.settings')
->>>>>>> 1314b6625889a883f0e68828555e2f29fc013e20
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
