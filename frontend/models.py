0c3333a3138b614e34778120fe53d0fdb2cbd26c
Andrew-64 <31630789+Andrew-64@users.noreply.github.com>
2018-11-10 12:46:26 -0500

    New Models & Tests

--- frontend/models.py
+++ frontend/models.py
@@ -7,42 +7,60 @@ from django.contrib.auth.models import AbstractUser
 
 
 class Student(AbstractUser):
-        student_name = models.CharField(max_length=200)
-        enter_time = models.DateTimeField(null = True)
-        student_computing_id = models.CharField(max_length=200)
-        student_location = models.CharField(max_length=200, default='')
-        password = models.CharField(max_length=50, null = True)
-        def __str__(self):
-                return self.student_name
+	student_name = models.CharField(max_length=200)
+	enter_time = models.DateTimeField(null = True)
+	student_computing_id = models.CharField(max_length=200)
+	student_location = models.CharField(max_length=200, default='')
+	password = models.CharField(max_length=50, null = True)
+	def __str__(self):
+		return self.student_name
 
-        #Takes in a Location model
-        def check_into(self, location):
-                self.enter_time = datetime.datetime.now()
-                self.student_location = location.check_in()
+	#Takes in a Location model and an estimate value
+	def check_into(self, location, estimate):
+		self.enter_time = datetime.datetime.now()
+		self.student_location = location.check_in(estimate)
 
-        def time_out(self):
-                elapsed_time = datetime.datetime.now() - self.enter_time
-                if(abs(elapsed_time) > datetime.timedelta(hours = 2)):
-                        l = Location.objects.get(location_name = self.student_location)
-                        l.check_out()
-                        self.student_location = ''
+	#Do we need this anymore?
+	def time_out(self):
+		elapsed_time = datetime.datetime.now() - self.enter_time
+		if abs(elapsed_time) > datetime.timedelta(hours = 3):
+			#l = Location.objects.get(location_name = self.student_location)
+			self.student_location = ''
 
 
 class Location(models.Model):
 	location_name=models.CharField(max_length=200)
-	maximum_capacity= models.IntegerField(default=0)
-	current_occupancy = models.IntegerField(default=0)
+
+	occupancy_list = []  #occupancy_list should contain tuples in the form of (estimated occupancy, time subitted)
+
 	def __str__(self):
 		return self.location_name
 
+	#Calculates average of all votes. Returns -1 if no votes
 	def get_percentage_full(self):
-		return self.current_occupancy/self.maximum_capacity
+		if len(self.occupancy_list) == 0:
+			return -1
+		percentage_full = 0
+		for x in self.occupancy_list:
+			percentage_full += x[0]
+		return percentage_full/len(self.occupancy_list)
 
-	def check_in(self):
-		self.current_occupancy += 1
+	#Estimation is the value given by the user when they are asked "How full is this space?"
+	def check_in(self, estimation):
+		estimate = (estimation, datetime.datetime.now())
+		self.occupancy_list.append(estimate)
 		return self.location_name
 
-	def check_out(self):
-		self.current_occupancy -= 1
-		if(self.current_occupancy < 0):
-			self.current_occupancy = 0
+	#Call this method every interval
+	def check_votes(self):
+		for x in self.occupancy_list:
+			if abs(datetime.datetime.now() - x[1]) > datetime.timedelta(hours = 3):  #Change this to set time votes get deleted
+				self.occupancy_list.remove(x)
+
+	def clear_votes(self):
+		self.occupancy_list.clear()
+
+	#Helper method to run test cases. Sets a manual time value in a tuple
+	@staticmethod
+	def manual_estimate_time(estimate, time):
+		return estimate, time
