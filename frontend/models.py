1af4a853a7758111c7d648025ade2352689578a2
Andrew-64 <31630789+Andrew-64@users.noreply.github.com>
2018-11-11 16:18:03 -0500

    Cleanup

--- frontend/models.py
+++ frontend/models.py
@@ -15,28 +15,28 @@ class Student(AbstractUser):
 	def __str__(self):
 		return self.student_name
 
-	#Takes in a Location model and an estimate value
+	# Takes in a Location model and an estimate value
 	def check_into(self, location, estimate):
 		self.enter_time = datetime.datetime.now()
 		self.student_location = location.check_in(estimate)
 
-	#Do we need this anymore?
+	# Do we need this anymore?
 	def time_out(self):
 		elapsed_time = datetime.datetime.now() - self.enter_time
 		if abs(elapsed_time) > datetime.timedelta(hours = 3):
-			#l = Location.objects.get(location_name = self.student_location)
+			# l = Location.objects.get(location_name = self.student_location)
 			self.student_location = ''
 
 
 class Location(models.Model):
 	location_name=models.CharField(max_length=200)
 
-	occupancy_list = []  #occupancy_list should contain tuples in the form of (estimated occupancy, time subitted)
+	occupancy_list = []  # occupancy_list should contain tuples in the form of (estimated occupancy, time subitted)
 
 	def __str__(self):
 		return self.location_name
 
-	#Calculates average of all votes. Returns -1 if no votes
+	# Calculates average of all votes. Returns -1 if no votes
 	def get_percentage_full(self):
 		if len(self.occupancy_list) == 0:
 			return -1
@@ -45,22 +45,22 @@ class Location(models.Model):
 			percentage_full += x[0]
 		return percentage_full/len(self.occupancy_list)
 
-	#Estimation is the value given by the user when they are asked "How full is this space?"
+	# Estimation is the value given by the user when they are asked "How full is this space?"
 	def check_in(self, estimation):
 		estimate = (estimation, datetime.datetime.now())
 		self.occupancy_list.append(estimate)
 		return self.location_name
 
-	#Call this method every interval
+	# Call this method every interval
 	def check_votes(self):
 		for x in self.occupancy_list:
-			if abs(datetime.datetime.now() - x[1]) > datetime.timedelta(hours = 3):  #Change this to set time votes get deleted
+			if abs(datetime.datetime.now() - x[1]) > datetime.timedelta(hours = 3):  # Change this to set time votes get deleted
 				self.occupancy_list.remove(x)
 
 	def clear_votes(self):
 		self.occupancy_list.clear()
 
-	#Helper method to run test cases. Sets a manual time value in a tuple
+	# Helper method to run test cases. Sets a manual time value in a tuple
 	@staticmethod
 	def manual_estimate_time(estimate, time):
 		return estimate, time
