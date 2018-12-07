3d6fbee53dfe845ba8f937ad8d1e75771f98fdff
Isaac <itr9fc@virginia.edu>
2018-12-07 12:50:01 -0500

    Display of cards
    View map detail cards are now formatted much better and have had their layout changed significantly

--- frontend/static/frontend/js/map.js
+++ frontend/static/frontend/js/map.js
@@ -16,6 +16,7 @@ var doOnce = false;
 var card = "<div class='card' style='width: 18rem;'> \n <img class='card-img-top'  alt='Card image cap'> \n<div class='card-body'>\n<p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>\n</div>\n</div>"
 var startvalue;
 var locname;
+var loctitle
 var descr
 function showDetail(x){ 
         console.log(x)
@@ -24,6 +25,7 @@ function showDetail(x){
             if(element.fields.location_name == x){
                 startvalue = element.fields.percent_full
                 locname = x
+                loctitle = element.fields.title
                 descr = element.fields.description
                 
             }
@@ -41,22 +43,31 @@ function showDetail(x){
 
         slider.style="visibility: visible;";
         var out = document.getElementById('out')
-        out.style = "visibility:visible;";
-        out.innerHTML = slider.defaultValue;
+        out.style = "visibility:visible; font-size:xx-large;";
+        out.innerHTML = slider.defaultValue + "% Full";
         var overalldiv = document.createElement('div')
         var test = document.createElement("h1")
         test.setAttribute('id', 'name');
-        test.innerHTML = locname
+        test.innerHTML = loctitle + " estimated capacity: " + slider.defaultValue + "% full"
 
         var cardele = document.createElement('div')
         cardele.setAttribute('id','desc')
-        cardele.innerHTML = "<div class='card' style='width: 18rem;'> \n <img class='card-img-top' id='imgchoose' alt='Card image cap'> \n<div class='card-body'>\n<p class='card-text'>" + descr + "</p>\n</div>\n</div>"
-        
+        cardele.innerHTML = "<div class='card' style='width: 18rem; display: inline-block'> \n <img class='card-img-top' id='imgchoose' alt='Card image cap'> \n<div class='card-body'>\n<p class='card-text'>" + descr + "</p>\n</div>\n</div>"
+
+
+         var capacity_text = document.createElement("h1")
+        capacity_text.setAttribute('id', 'capacity_header');
+        var temp_value = "Submit a new estimate";
+        capacity_text.innerHTML = temp_value;
+
 
         overalldiv.appendChild(test)
-        overalldiv.appendChild(slider)
-        overalldiv.appendChild(out)
         overalldiv.appendChild(cardele)
+
+        overalldiv.appendChild(capacity_text)
+         overalldiv.appendChild(slider)
+        overalldiv.appendChild(out)
+
         
         overalldiv.style = "margin-top: 4cm;";
 
@@ -141,15 +152,14 @@ function showDetail(x){
             imgchoose.src = "../../static/frontend/images/universityofvirginiaschooloflaw.jpg"
 }
 
-        var ret = document.createElement('button');
-        ret.setAttribute('class', 'accordion');
-        ret.innerHTML = "go back";
-        ret.addEventListener('click', removeDetails);
+//       var capacity_text = document.createElement('capacity_header')
+//       capacity_text.set
 
-        
+
+
         var submit = document.createElement('input')
         submit.setAttribute('type', 'submit')
-        submit.setAttribute('value', 'Submit Occupancy!')
+        submit.setAttribute('value', 'Submit New Estimate!')
 
         var inputname = document.getElementById('location')
         inputname.setAttribute('type', 'hidden')
@@ -160,7 +170,7 @@ function showDetail(x){
 
 
 
-        overalldiv.appendChild(ret)
+
         overalldiv.appendChild(submit)
         overalldiv.appendChild(inputname)
 
@@ -168,10 +178,10 @@ function showDetail(x){
     }
     else{
         var cardele = document.getElementById('desc');
-        cardele.innerHTML = "<div class='card' style='width: 18rem;'> \n <img class='card-img-top'  alt='Card image cap'> \n<div class='card-body'>\n<p class='card-text'>" + descr + "</p>\n</div>\n</div>"
+        cardele.innerHTML = "<div class='card' style='width: 18rem; display: inline-block'> \n <img class='card-img-top'  alt='Card image cap'> \n<div class='card-body'>\n<p class='card-text'>" + descr + "</p>\n</div>\n</div>"
         var test = document.getElementById("name")
         test.setAttribute('id', 'name');
-        test.innerHTML = locname
+        test.innerHTML = loctitle
         var imgchoose = document.getElementsByClassName('card-img-top')[0]
         if(x == "Clemons" || x == "clemons"){
             imgchoose.src = "../../static/frontend/images/Clemons.jpg"
@@ -257,7 +267,7 @@ function showDetail(x){
         slider.setAttribute('name', 'myRange')
 
         var out = document.getElementById('out')
-        out.innerHTML = slider.defaultValue;
+        out.innerHTML = slider.defaultValue + "% Full";
 
         var inputname = document.getElementById('location')
         inputname.setAttribute('type', 'hidden')
@@ -284,6 +294,7 @@ function removeDetails(){
     var createOutput = document.createElement('p')
     createOutput.style = "visibility:hidden;";
     createOutput.setAttribute('id', 'out');
+    createOutput.setAttribute('style', 'font size = xx-large' )
     var inputname = document.createElement('input')
     inputname.setAttribute('id', 'location')
     inputname.setAttribute('type', 'hidden')
@@ -304,8 +315,8 @@ function removeDetails(){
 
 var slider2 = document.getElementById("myRange");
 var output = document.getElementById("out");
-output.innerHTML = slider2.value;
+output.innerHTML = slider2.value + "% Full";
 
 slider2.oninput = function() {
-output.innerHTML = this.value;
+output.innerHTML = this.value + "% Full";
 }
